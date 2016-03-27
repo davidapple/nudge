50f63e173309d7b758a1abec90fd65e9866d6c4f
David Apple <davidapple@protonmail.com>
2016-03-27 20:20:57 +0100

    Grunt developed to convert less to sass.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,62 @@ module.exports = function(grunt) {
     less: {
       development: {
         options: {
-          paths: ["dist/css"]
+          paths: ['dist/css']
         },
-        files: {"dist/css/style.css": "less/style.less"}
+        files: {'dist/css/nudge.css': 'less/nudge.less'}
       },
       production: {
         options: {
-          paths: ["dist/css"],
+          paths: ['dist/css'],
           cleancss: true
         },
-        files: {"dist/css/style.css": "less/style.less"}
+        files: {'dist/css/nudge.css': 'less/nudge.less'}
+      }
+    },
+    lessToSass: {
+      convert: {
+        files: [{
+          expand: true,
+          cwd: 'less',
+          src: ['*.less'],
+          ext: '.scss',
+          dest: 'sass'
+        }],
+        options: {
+          replacements: [{
+            pattern: /(\s+)\.([\w\-]*)\s*\((.*)\);/gi,
+            replacement: '$1@include $2($3)',
+            order: 2
+          }]
+        }
+      }
+    },
+    replace: {
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: /~''/g,
+              replacement: 'null'
+            }
+          ]
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['sass/*.scss'],
+          dest: 'sass/'
+        }]
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'dist/css/nudge.css': 'sass/nudge.scss'
+        }
       }
     },
     cssmin: {
@@ -33,8 +79,15 @@ module.exports = function(grunt) {
       },
     }
   });
+  grunt.loadNpmTasks('grunt-less-to-sass');
+  grunt.loadNpmTasks('grunt-replace');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['less', 'cssmin']);
+  grunt.registerTask('doLess', 'less');
+  grunt.registerTask('doConvert', ['lessToSass', 'replace']);
+  grunt.registerTask('doSass', 'sass');
+  grunt.registerTask('doMin', 'cssmin');
+  grunt.registerTask('default', ['less', 'lessToSass', 'replace', 'sass', 'cssmin']);
 };
